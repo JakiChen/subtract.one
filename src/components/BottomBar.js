fd9e5203b16faf8cba98fa7d9b92d36900f8ba76
jakichen <jakichen@live.cn>
2020-08-08 00:16:04 +0800

    Update BottomBar.js

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -127,7 +127,7 @@ function BottomBar({ onboardingReset, setPatchLink, show, sharePatch }) {
 							rel="noopener noreferrer"
 							href="https://www.linkedin.com/in/jakichen/"
 						>
-							Jaki Chen
+							Jaki Chen - Action test
 						</a>
 					</Item>,
 					<Item {...animate}>
