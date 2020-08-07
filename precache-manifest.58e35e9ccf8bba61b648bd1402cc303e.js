7a15d24953fb16bdb6977a49aa07a95e92167665
Jaki Chen <JakiChen@live.cn>
2020-08-07 20:13:37 +0800

    Updates

---
+++ precache-manifest.58e35e9ccf8bba61b648bd1402cc303e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bb3bf6d4439f03631e3ec4db0769d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b1db41440038ec36d11",
+    "url": "./static/css/main.e8d7751c.chunk.css"
+  },
+  {
+    "revision": "bdeb102471b8bbd40fa8",
+    "url": "./static/js/2.ada3e83c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "./static/js/2.ada3e83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1db41440038ec36d11",
+    "url": "./static/js/main.01681809.chunk.js"
+  },
+  {
+    "revision": "bb39276cdef18fb31f38",
+    "url": "./static/js/runtime-main.615cdb9b.js"
+  }
+]);
